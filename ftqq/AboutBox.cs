ca440ce7206aa221071342c253c882c1a50d4812
MoGeek <zhangyueyi_java@outlook.com>
2019-02-16 16:06:10 +0800

    大致功能完成，测试发送消息成功。

---
+++ ftqq/AboutBox.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace ftqq
+{
+    partial class AboutBox : Form
+    {
+        public AboutBox()
+        {
+            InitializeComponent();
+            //this.Text = String.Format("关于1 {0}", AssemblyTitle);
+            this.Text = "关于方糖气球";
+            //this.labelProductName.Text = AssemblyProduct + "2";
+            this.labelProductName.Text = "Server 酱:";
+            //this.labelVersion.Text = String.Format("版本3 {0}", AssemblyVersion);
+            this.labelVersion.Text = "http://sc.ftqq.com";
+            //this.labelCopyright.Text = AssemblyCopyright + "4";
+            this.labelCopyright.Text = "";
+            //this.labelCompanyName.Text = AssemblyCompany + "5";
+            this.labelCompanyName.Text = "";
+            //this.textBoxDescription.Text = AssemblyDescription + "6";
+            this.textBoxDescription.Text = "在 SCKEY 栏填入对应的参数即可发送信息到微信，\n详细的信息请点击上方网址获取。";
+        }
+
+        #region 程序集特性访问器
+
+        public string AssemblyTitle
+        {
+            get
+            {
+                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyTitleAttribute), false);
+                if (attributes.Length > 0)
+                {
+                    AssemblyTitleAttribute titleAttribute = (AssemblyTitleAttribute)attributes[0];
+                    if (titleAttribute.Title != "")
+                    {
+                        return titleAttribute.Title;
+                    }
+                }
+                return System.IO.Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().CodeBase);
+            }
+        }
+
+        public string AssemblyVersion
+        {
+            get
+            {
+                return Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            }
+        }
+
+        public string AssemblyDescription
+        {
+            get
+            {
+                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyDescriptionAttribute), false);
+                if (attributes.Length == 0)
+                {
+                    return "";
+                }
+                return ((AssemblyDescriptionAttribute)attributes[0]).Description;
+            }
+        }
+
+        public string AssemblyProduct
+        {
+            get
+            {
+                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyProductAttribute), false);
+                if (attributes.Length == 0)
+                {
+                    return "";
+                }
+                return ((AssemblyProductAttribute)attributes[0]).Product;
+            }
+        }
+
+        public string AssemblyCopyright
+        {
+            get
+            {
+                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false);
+                if (attributes.Length == 0)
+                {
+                    return "";
+                }
+                return ((AssemblyCopyrightAttribute)attributes[0]).Copyright;
+            }
+        }
+
+        public string AssemblyCompany
+        {
+            get
+            {
+                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCompanyAttribute), false);
+                if (attributes.Length == 0)
+                {
+                    return "";
+                }
+                return ((AssemblyCompanyAttribute)attributes[0]).Company;
+            }
+        }
+        #endregion
+
+        private void LabelVersion_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("explorer.exe", "http://sc.ftqq.com");
+            OkButton_Click(sender, e);
+        }
+
+        private void OkButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
--- ftqq/MainWindow.xaml.cs
+++ ftqq/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.IO;
+using System.Net;
 
 namespace ftqq
 {
@@ -22,7 +13,67 @@ namespace ftqq
     {
         public MainWindow()
         {
+
             InitializeComponent();
+
+
+        }
+
+        private void Bt_send_Click(object sender, RoutedEventArgs e)
+        {
+            Tb_result_TextChange("wait");
+            string result = HttpPost(tb_text.Text, tb_desp.Text);
+            Tb_result_TextChange(result);
+        }
+
+        private string HttpPost(string tb_text, string tb_desp)
+        {
+            var request = (HttpWebRequest)WebRequest.Create("https://sc.ftqq.com/" + tb_sckey.Text + ".send");
+
+            var postData = "text=" + tb_text;
+            postData += "&desp=" + tb_desp;
+            var data = Encoding.UTF8.GetBytes(postData);
+
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded;charset=utf8";
+            request.ContentLength = data.Length;
+
+            using (var stream = request.GetRequestStream())
+            {
+                stream.Write(data, 0, data.Length);
+            }
+
+            var response = (HttpWebResponse)request.GetResponse();
+
+            var responeString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+            return responeString;
+            //throw new NotImplementedException();
+        }
+
+        private void Tb_result_TextChange(string text)
+        {
+            switch (text)
+            {
+                case "wait":
+                    tb_result.Text = "wait...";
+                    tb_result.Foreground = new SolidColorBrush(Colors.Black);
+                    break;
+                case "{\"errno\":0,\"errmsg\":\"success\",\"dataset\":\"done\"}":
+                    tb_result.Text = "Success!";
+                    tb_result.Foreground = new SolidColorBrush(Colors.Green);
+                    break;
+                default:
+                    tb_result.Text = text;
+                    tb_result.Foreground = new SolidColorBrush(Colors.Red);
+                    break;
+            }
+
+        }
+
+        private void Bt_about_Click(object sender, RoutedEventArgs e)
+        {
+            AboutBox about = new AboutBox();
+            about.Show();
         }
     }
 }
